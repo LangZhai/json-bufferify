4e9787d68f022b456ff234fdc507141de0c4a5ee
智能小菜菜 <zl2012xyz@hotmail.com>
2020-10-13 16:19:53 +0800

    Update LICENSE

--- dist/json-bufferify.js
+++ dist/json-bufferify.js
@@ -3,7 +3,7 @@
  * Date: 2017-11-13
  * © 2017 LangZhai(智能小菜菜)
  * This is licensed under the GNU LGPL, version 3 or later.
- * For details, see: http://www.gnu.org/licenses/lgpl.html
+ * For details, see: https://www.gnu.org/licenses/lgpl.html
  * Project home: https://github.com/LangZhai/json-bufferify
  */
 
