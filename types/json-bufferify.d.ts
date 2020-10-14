a3f6faa9069ca14069955730f425084db3c927e8
智能小菜菜 <zl2012xyz@hotmail.com>
2020-10-14 17:40:32 +0800

    Update to 0.2.1.

--- types/json-bufferify.d.ts
+++ types/json-bufferify.d.ts
@@ -1,6 +1,6 @@
 /**
- * json-bufferify 0.2.0
- * Date: 2020-10-13
+ * json-bufferify 0.2.1
+ * Date: 2020-10-14
  * © 2017-2020 LangZhai(智能小菜菜)
  * This is licensed under the GNU LGPL, version 3 or later.
  * For details, see: https://www.gnu.org/licenses/lgpl.html
